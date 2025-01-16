a2bbf692a4f8ba7dd9bef427a36d7f759ae5b5a9
Zairth <mano.gimenez@hotmail.com>
2025-01-16 10:08:56 +0100

    Try to fix error

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 print("Last digit of {0} is".format(number), end=" ")
-if number < 0:
-    number *= -1
-number %= 10
-print("{0} and is".format(number), end=" ")
-if number > 5:
+last_digit = number
+if last_digit < 0:
+    last_digit *= -1
+last_digit %= 10
+print("{0} and is".format(last_digit), end=" ")
+if last_digit > 5:
     print("greater than 5")
-elif number == 0:
+elif last_digit == 0:
     print("0")
-elif number < 6 and number != 0:
+elif last_digit < 6 and last_digit != 0:
     print("less than 6 and not 0")

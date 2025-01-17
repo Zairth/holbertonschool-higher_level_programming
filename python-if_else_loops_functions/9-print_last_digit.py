76e1333814cfd3eac9f6df273499365323eaa880
Zairth <mano.gimenez@hotmail.com>
2025-01-17 09:21:09 +0100

    Try to fix error

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    str_number = str(number)
-    last_digit = str_number[-1]
-    print("{}".format(last_digit), end="")
-    return int(last_digit)
+    number = abs(number)
+    number %= 10
+    print("{}".format(number), end="")
+    return number

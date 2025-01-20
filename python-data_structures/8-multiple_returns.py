e66d4f09bb880d9041aa860023b0b9c9a73f7c86
Zairth <mano.gimenez@hotmail.com>
2025-01-20 17:39:23 +0100

    Adding task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,9 +2,10 @@
 def multiple_returns(sentence):
     _len = len(sentence)
     first_char = ''
-
     if _len != 0:
         first_char = sentence[0]
+    else:
+        first_char = "None"
 
     tuple = (_len, first_char)
 

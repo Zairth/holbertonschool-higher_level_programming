34e973c334fb1337ecd48ae38e34755e62098fb4
Zairth <mano.gimenez@hotmail.com>
2025-01-20 18:09:33 +0100

    Adding task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
-    if len(my_list) >= idx + 1 and idx > 0:
+    if len(my_list) >= idx + 1 and idx >= 0:
         my_list.remove(idx + 1)
 
     return my_list

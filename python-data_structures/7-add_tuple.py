60047aa79fb6a0c126b04d7b5f83d4fd0ff38ca3
Zairth <mano.gimenez@hotmail.com>
2025-01-20 17:31:25 +0100

    Adding task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple_a = [0, 0]
-    new_tuple_b = [0, 0]
+    nw_tuple_a = [0, 0]
+    nw_tuple_b = [0, 0]
     new_tuple = ()
 
     for i in range(2):
         if len(tuple_a) - 1 < i:
-            new_tuple_a[i] = 0
+            nw_tuple_a[i] = 0
         else:
-            new_tuple_a[i] = tuple_a[i]
+            nw_tuple_a[i] = tuple_a[i]
         if len(tuple_b) - 1 < i:
-            new_tuple_b[i] = 0
+            nw_tuple_b[i] = 0
         else:
-            new_tuple_b[i] = tuple_b[i]
+            nw_tuple_b[i] = tuple_b[i]
 
-    new_tuple = new_tuple_a[0] + new_tuple_b[0], new_tuple_a[1] + new_tuple_b[1]
+    new_tuple = nw_tuple_a[0] + nw_tuple_b[0], nw_tuple_a[1] + nw_tuple_b[1]
 
     return new_tuple

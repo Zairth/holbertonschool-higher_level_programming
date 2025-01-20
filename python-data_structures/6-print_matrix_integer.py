3b62514f692438d2f2dea1635679d62c1acab74d
Zairth <mano.gimenez@hotmail.com>
2025-01-20 16:41:43 +0100

    Adding task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for integer in list:
-            print("{}".format(integer), end=" ")
+            print("{:d}".format(integer), end=" ")
         print("\n", end="")

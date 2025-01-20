f4207bddc4315c8ece2c55c917846029f87f29e3
Zairth <mano.gimenez@hotmail.com>
2025-01-20 18:42:15 +0100

    Deleting the last space after the print to get the exact same output as the exemple for the task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
-        for integer in list:
-            print("{:d}".format(integer), end=" ")
+        for i in range(len(list)):
+            if i + 1 > len(list) - 1:
+                print("{:d}".format(list[i]), end="")
+            else:
+                print("{:d}".format(list[i]), end=" ")
         print("\n", end="")

0b76fe7b977657ef2e3aeb731c6cc55f4166f276
Zairth <mano.gimenez@hotmail.com>
2025-01-21 15:20:08 +0100

    Adding task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    best_value = ""
+
     if a_dictionary is not None:
-        first_key = list(a_dictionary.keys())[0]
-        max_integer = a_dictionary[first_key]
-        best_value = first_key
+
+        max_integer = 0
 
         for cle, valeur in a_dictionary.items():
             if valeur > max_integer:
@@ -11,4 +12,5 @@ def best_score(a_dictionary):
                 best_value = cle
 
         return best_value
+
     return None

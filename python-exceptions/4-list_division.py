390050300829a714ac6a9590847fb6b49e737f30
Zairth <mano.gimenez@hotmail.com>
2025-01-23 13:51:37 +0100

    Adding task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    resultat = list()
+    for i in range(list_length):
+        try:
+            resultat.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            resultat.append(0)
+            continue
+        except ZeroDivisionError:
+            print("division by 0")
+            resultat.append(0)
+            continue
+        except IndexError:
+            print("out of range")
+            resultat.append(0)
+            continue
+
+    return resultat

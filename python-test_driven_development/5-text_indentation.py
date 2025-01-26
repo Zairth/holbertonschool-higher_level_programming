5a54d0d1e3b0811b57bc040eeda3a79963b6415c
Zairth <mano.gimenez@hotmail.com>
2025-01-26 23:05:33 +0100

    Adding task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Print un texte
+"""
+
+
+def text_indentation(text):
+    """
+    Print un text et faire des sauts de ligne en fonction de caractere spéciaux
+    Args:
+        text (str): le text à print
+    Returns:
+        None
+    Exemple:
+        >>> text_indentation("Bonjour. Je vais: Bien?")
+        Bonjour.
+        <BLANKLINE>
+        Je vais:
+        <BLANKLINE>
+        Bien?
+        <BLANKLINE>
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    forbidden_char = ['.', '?', ':']
+    forbidden_char_found = False
+
+    for i in range(len(text)):
+        if forbidden_char_found:
+            forbidden_char_found = False
+            continue
+        print("{}".format(text[i]), end="")
+        for j in range(len(forbidden_char)):
+            if text[i] == forbidden_char[j]:
+                print("\n")
+                forbidden_char_found = True
+                break

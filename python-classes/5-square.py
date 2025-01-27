107cd5f9866e5a411b70fe569ac35918533f122d
Zairth <mano.gimenez@hotmail.com>
2025-01-27 16:51:59 +0100

    Adding task 6

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -12,7 +12,7 @@ class Square:
         size (int): instance attribute size of the square
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Init the square with a size
 
@@ -21,6 +21,7 @@ class Square:
         """
 
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -43,6 +44,31 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = __size
 
+    @property
+    def position(self):
+        """
+        Getter to gain acces of __size
+
+        Returns:
+            The size of the square
+        """
+
+        return self.__position
+
+    @position.setter
+    def position(self, __position):
+        """Setter to modificate __size with verification"""
+
+        for tuple in __position:
+            if not isinstance(tuple, int):
+                raise TypeError("position must be a tuple of \
+2 positive integers")
+            if tuple < 0:
+                raise TypeError("position must be a tuple of \
+2 positive integers")
+
+        self.__position = __position
+
     def area(self):
         """
         Returning the area of the square
@@ -58,10 +84,15 @@ class Square:
         Print a square
         """
 
+        for i in range(self.position[1]):
+            print("")
+
         if self.size == 0:
             print("")
         else:
             for i in range(self.size):
+                for i in range(self.position[0]):
+                    print(" ", end="")
                 for j in range(self.size):
                     print("#", end="")
                 print("")

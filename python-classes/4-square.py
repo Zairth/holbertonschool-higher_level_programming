5ecc5f17d44086ee086a649fb56950dbb1f62d15
Zairth <mano.gimenez@hotmail.com>
2025-01-27 14:07:00 +0100

    Adding task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -52,16 +52,3 @@ class Square:
         """
 
         return self.__size * self.__size
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)

005607382317c4748abebccf6c5b49bd407a4d0c
Zairth <mano.gimenez@hotmail.com>
2025-01-27 13:03:10 +0100

    Adding task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Create a square
+"""
+
+
+class Square:
+    """
+    Create a new square
+
+    Attributes:
+        __size (int): Private instance attribute size of the square
+    """
+
+    def __init__(self, __size):
+        self.__size = __size

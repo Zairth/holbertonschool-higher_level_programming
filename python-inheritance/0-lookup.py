33d1be49ea673b98686869a599a728aeb433c2ca
Zairth <mano.gimenez@hotmail.com>
2025-02-03 11:38:15 +0100

    Adding task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,12 @@
 
 """
 Function that return a list of available attribute and method of an object
+
+Args:
+    obj: The object to return the list
+
+Returns:
+    a dir of available attributes and methods of the args
 """
 
 

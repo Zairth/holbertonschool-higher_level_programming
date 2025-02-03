1907484e954ed24aa17ad45bc984d1f5c35cae12
Zairth <mano.gimenez@hotmail.com>
2025-02-03 11:48:51 +0100

    Adding task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,5 +2,13 @@
 
 
 def lookup(obj):
-    """ returns the list of available attributes and methods of an object """
+    """Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object whose attributes and methods are to be returned.
+
+    Returns:
+        A list of string representing the attributes and methods of the object.
+    """
+
     return list(dir(obj))

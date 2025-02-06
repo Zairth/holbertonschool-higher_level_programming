b442a28f26a43fd4fdf0d3dca2ba0f272c3e8312
Zairth <mano.gimenez@hotmail.com>
2025-02-07 00:17:33 +0100

    Adding task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,24 +3,7 @@
 """
 
 
-class BaseGeometry():
-    """
-    Create a new instance
-    """
-
-    def area(self):
-        """Raise an exception"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer."""
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -37,5 +20,6 @@ class Rectangle(BaseGeometry):
 
         self.integer_validator("height", height)
         self.integer_validator("width", width)
+
         self.__height = height
         self.__width = width

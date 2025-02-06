ad52d91a37fdf115502e138eda38a86baefd33e9
Zairth <mano.gimenez@hotmail.com>
2025-02-06 16:59:10 +0100

    Adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""6-base_geometry.py
+"""
+
+
+class BaseGeometry():
+    """
+    Create a new instance
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")

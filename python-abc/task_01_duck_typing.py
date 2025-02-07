fe95df712a1eb43a6a58cc0f02e8b5d162cab9e0
Zairth <mano.gimenez@hotmail.com>
2025-02-07 19:29:04 +0100

    Adding task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Abstract class inherited from ABC"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """Class inherited from Animal"""
+
+    def __init__(self, radius):
+        """Init the instance with Radius"""
+
+        radius = abs(radius)
+        self.radius = radius
+
+    def area(self):
+        """Abstract method that implemente the parent method"""
+
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        """Abstract method that implemente the parent method"""
+
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """Class inherited from Animal"""
+
+    def __init__(self, height, width):
+        """Init the instance with Height, and width"""
+
+        self.height = height
+        self.width = width
+
+    def area(self):
+        """Abstract method that implemente the parent method"""
+
+        return 2 * (self.height + self.width)
+
+    def perimeter(self):
+        """Abstract method that implemente the parent method"""
+
+        return self.height * self.width

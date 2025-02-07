186324db50dd0aa7a839dd8b8ee5adabde8f2225
Zairth <mano.gimenez@hotmail.com>
2025-02-07 10:03:42 +0100

    Adding task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""11-square.py
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Create a new instance inherited from Rectangle
+    """
+
+    def __init__(self, size):
+        """Init the Square"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return a personnalized string"""
+
+        return f"[Square] {self.__size}/{self.__size}"

178c9704782e3a1c8456a5e2994d7e36522d612a
Zairth <mano.gimenez@hotmail.com>
2025-02-10 09:52:28 +0100

    Adding task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 def read_file(filename=""):
     """Open, Read, And write a file in the stdout"""
 
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print("{}".format(file.read()), end="")

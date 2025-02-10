013e1531b0a4c8033186851ee5c1d606c334390f
Zairth <mano.gimenez@hotmail.com>
2025-02-10 10:36:57 +0100

    Adding task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Convert a Python data into JSON format"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return a Python data into JSON format"""
+
+    return json.dumps(my_obj)

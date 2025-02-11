fcd688fba726dfef13659eaff9edc9c75dddbe61
Zairth <mano.gimenez@hotmail.com>
2025-02-11 10:52:31 +0100

    Adding task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-import os
-import sys
-
-
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 """My class Student"""
 
 

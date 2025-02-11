13ea1799855367acd110947b012ff9c912145219
Zairth <mano.gimenez@hotmail.com>
2025-02-11 13:47:21 +0100

    Adding task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""task_01_pickle.py"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """Convert csv into json"""
+
+    try:
+        new_data = []
+
+        with open(filename, mode='r') as file:
+            data = csv.DictReader(file)
+
+            for row in data:
+                new_data.append(row)
+
+        with open("data.json", "w", encoding="utf-8") as file:
+            json.dump(new_data, file)
+
+        return True
+
+    except Exception as e:
+        print("Error during the conversion: {}".format(e))
+
+        return None

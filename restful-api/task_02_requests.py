b444da0a1a40b0f87153be9b6b951f5d910039f3
Zairth <mano.gimenez@hotmail.com>
2025-02-18 11:31:57 +0100

    Adding task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""task_02_requests.py"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """Fetch and print a post"""
+
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    if response.status_code == 200:
+
+        print("Status Code: 200")
+
+        posts = response.json()
+
+        for post in posts:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    """Fetch and save a post"""
+
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        posts = response.json()
+
+        post_data = list()
+
+        for post in posts:
+            post_data.append
+            ({"id": post["id"], "title": post["title"], "body": post["body"]})
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(post_data)
+
+
+fetch_and_print_posts()
+fetch_and_save_posts()

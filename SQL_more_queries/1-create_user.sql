29c1f293a25c697bcb318a16541447488a31af4e
Zairth <mano.gimenez@hotmail.com>
2025-03-05 16:13:08 +0100

    gg

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS
+'user_0d_1'@'localhost' IDENTIFIED BY 'MonMotDePasse@123';
+GRANT ALL PRIVILEGES ON *.* TO
+'user_0d_1'@'localhost';
+FLUSH PRIVILEGES

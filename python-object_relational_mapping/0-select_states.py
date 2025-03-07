249ca03a21d824a02f211277a794dc83446de1f5
Zairth <mano.gimenez@hotmail.com>
2025-03-07 21:04:40 +0100

    gg

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,12 +8,16 @@ import sys
 
 if __name__ == "__main__":
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        db=sys.argv[3]
+        host = "localhost",
+        port = 3306,
+        user = username,
+        password = password,
+        db = database_name
     )
 
     cur = db.cursor()

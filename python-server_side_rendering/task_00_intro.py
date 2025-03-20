d8b4b4f74bfd4cab558f0f008588f3446bfd3398
Zairth <mano.gimenez@hotmail.com>
2025-03-20 12:54:05 +0100

    gg

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -12,16 +12,22 @@ def generate_invitations(template, attendees):
     """
 
     if not isinstance(template, str):
-        raise TypeError(f"Invalid input: template must be a string, but got {type(template).__name__}.")
+        raise TypeError(f"Invalid input: template must\
+        be a string, but got {type(template).__name__}.")
+
     if len(template) == 0:
         raise ValueError("Template is empty, no output files generated.")
+
     if not isinstance(attendees, list):
-        raise TypeError(f"Invalid input: attendees must be a list, but got {type(attendees)}.")
+        raise TypeError(f"Invalid input: \
+        attendees must be a list, but got {type(attendees)}.")
+
     if not attendees:
         raise ValueError("No data provided, no output files generated.")
     for a in attendees:
         if not isinstance(a, dict):
-            raise TypeError(f"Invalid input: each attendee must be a dictionary, but got {type(a).__name__}.")
+            raise TypeError(f"Invalid input: \
+            each attendee must be a dictionary, but got {type(a).__name__}.")
 
     for key, value in a.items():
         if value is None or len(value.strip()) == 0:
@@ -39,7 +45,8 @@ def generate_invitations(template, attendees):
             output_filename = f"output_{i}.txt"
 
             if os.path.exists(output_filename):
-                raise FileExistsError(f"File {output_filename} already exists.")
+                raise FileExistsError(
+                    f"File {output_filename} already exists.")
 
             with open(f"output_{i}.txt", "w") as invitation:
                 invitation.write(personalized_invitation)

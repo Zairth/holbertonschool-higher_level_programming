bfcf3c89f793bda7e129c5049b13a18d4be2cbca
Zairth <mano.gimenez@hotmail.com>
2025-03-20 12:21:24 +0100

    gg

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+create a Python function that generates personalized invitation 
+files from a template with placeholders and a list of objects
+"""
+
+
+def generate_invitations(template, attendees):
+    """
+    Generate invitation from a template with attendees attributes
+    """
+
+    if not isinstance(template, str):
+        raise TypeError(f"Invalid input: template must be a string, but got {type(template).__name__}.")
+    if len(template) == 0:
+        raise ValueError("Template is empty, no output files generated.")
+    if not isinstance(attendees, list):
+        raise TypeError(f"Invalid input: attendees must be a list, but got {type(attendees)}.")
+    if not attendees:
+        raise ValueError("No data provided, no output files generated.")
+    for a in attendees:
+        if not isinstance(a, dict):
+            raise TypeError(f"Invalid input: each attendee must be a dictionary, but got {type(a).__name__}.")
+
+
+    for i, attendee in enumerate(attendees, start=1):
+        personalized_invitation = template.format(
+            name = attendee.get('name', 'N/A'),
+            event_title = attendee.get('event_title', 'N/A'),
+            event_date = attendee.get('event_date', 'N/A'),
+            event_location = attendee.get('event_location', 'N/A')
+        )
+
+        with open(f"output_{i}.txt", "w") as invitation:
+            invitation.write(personalized_invitation)
+
+
+# Read the template from a file
+with open('template.txt', 'r') as file:
+    template_content = file.read()
+
+# List of attendees
+attendees = [
+    {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"},
+    {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"},
+    {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
+]
+
+# Call the function with the template and attendees list
+generate_invitations(template_content, attendees)
